8e65bacde415860fd3e29a402d3cd9056a4e187f
Helium <helium314@disroot.org>
2021-08-11 22:10:46 +0200

    add phone and website quests

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/contact/AddContactPhoneForm.kt
@@ -0,0 +1,35 @@
+package de.westnordost.streetcomplete.quests.contact
+
+import android.os.Bundle
+import android.text.InputType
+import android.view.View
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.quests.AbstractQuestFormAnswerFragment
+import de.westnordost.streetcomplete.util.TextChangedWatcher
+import kotlinx.android.synthetic.main.quest_contact.*
+
+
+class AddContactPhoneForm : AbstractQuestFormAnswerFragment<String>() {
+
+    override val contentLayoutResId = R.layout.quest_contact
+
+    private val contact get() = nameInput?.text?.toString().orEmpty().trim()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        nameInput.inputType = InputType.TYPE_CLASS_PHONE
+
+        nameInput.setText(PREFILL)
+        nameInput.addTextChangedListener(TextChangedWatcher { checkIsFormComplete() })
+    }
+
+    override fun onClickOk() {
+        applyAnswer(contact)
+    }
+
+
+    override fun isFormComplete() = contact.isNotEmpty() && contact != PREFILL
+}
+
+private const val PREFILL = "+"
