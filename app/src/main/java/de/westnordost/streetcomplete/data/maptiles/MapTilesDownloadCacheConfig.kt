365b097d3349423d2777cec7238faa115155c14a
Helium <helium314@disroot.org>
2021-08-17 19:50:53 +0200

    extend map tile cache from 12 hours to 8 days

--- app/src/main/java/de/westnordost/streetcomplete/data/maptiles/MapTilesDownloadCacheConfig.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/maptiles/MapTilesDownloadCacheConfig.kt
@@ -15,7 +15,7 @@ import javax.inject.Singleton
 @Singleton class MapTilesDownloadCacheConfig @Inject constructor(context: Context) {
 
     val cacheControl = CacheControl.Builder()
-        .maxAge(12, TimeUnit.HOURS)
+        .maxAge(8, TimeUnit.DAYS)
         .maxStale(DELETE_OLD_DATA_AFTER.toInt(), TimeUnit.MILLISECONDS)
         .build()
 
