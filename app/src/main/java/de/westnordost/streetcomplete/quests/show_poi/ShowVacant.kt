50da69c0ea01c15d24e24511dfb4d6a0252a891f
Helium <helium314@disroot.org>
2021-08-27 10:45:29 +0200

    show vacant shop poi quest now allows replacing shops

--- app/src/main/java/de/westnordost/streetcomplete/quests/show_poi/ShowVacant.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/show_poi/ShowVacant.kt
@@ -1,11 +1,19 @@
 package de.westnordost.streetcomplete.quests.show_poi
 
 import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.meta.KEYS_THAT_SHOULD_NOT_BE_REMOVED_WHEN_SHOP_IS_REPLACED
+import de.westnordost.streetcomplete.data.meta.LAST_CHECK_DATE_KEYS
+import de.westnordost.streetcomplete.data.meta.SURVEY_MARK_KEY
+import de.westnordost.streetcomplete.data.meta.toCheckDateString
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapChangesBuilder
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
-import de.westnordost.streetcomplete.quests.NoAnswerFragment
+import de.westnordost.streetcomplete.quests.shop_type.IsShopVacant
+import de.westnordost.streetcomplete.quests.shop_type.ShopType
+import de.westnordost.streetcomplete.quests.shop_type.ShopTypeAnswer
+import de.westnordost.streetcomplete.quests.shop_type.ShopTypeForm
+import java.time.LocalDate
 
-class ShowVacant : OsmFilterQuestType<Boolean>() {
+class ShowVacant : OsmFilterQuestType<ShopTypeAnswer>() {
     override val elementFilter = """
         nodes, ways, relations with
         shop = vacant
@@ -14,17 +22,49 @@ class ShowVacant : OsmFilterQuestType<Boolean>() {
         or disused:amenity
         or disused:office
     """
-    override val commitMessage = "Change shop/business"
-    override val wikiLink = "nope"
+    override val commitMessage = "Check if vacant shop is still vacant"
+    override val wikiLink = "Key:disused:"
     override val icon = R.drawable.ic_quest_wheelchair_shop
     override val dotColor = "grey"
-    override val isReplaceShopEnabled = true
 
     override fun getTitle(tags: Map<String, String>) =
         R.string.quest_thisIsVacant_title
 
-    override fun createForm() = NoAnswerFragment()
+    override fun createForm() = ShopTypeForm()
 
-    override fun applyAnswerTo(answer: Boolean, changes: StringMapChangesBuilder) {
+    override fun getTitleArgs(tags: Map<String, String>, featureName: Lazy<String?>): Array<String> {
+        return arrayOf(tags.entries.toString())
+    }
+
+    override fun applyAnswerTo(answer: ShopTypeAnswer, changes: StringMapChangesBuilder) {
+        val otherCheckDateKeys = LAST_CHECK_DATE_KEYS.filterNot { it == SURVEY_MARK_KEY }
+        for (otherCheckDateKey in otherCheckDateKeys) {
+            changes.deleteIfExists(otherCheckDateKey)
+        }
+        when (answer) {
+            is IsShopVacant -> {
+                changes.addOrModify(SURVEY_MARK_KEY, LocalDate.now().toCheckDateString())
+            }
+            is ShopType -> {
+                if (!answer.tags.containsKey("shop")) {
+                    changes.deleteIfExists("shop")
+                }
+
+                changes.deleteIfExists(SURVEY_MARK_KEY)
+
+                for ((key, _) in changes.getPreviousEntries()) {
+                    // also deletes all "disused:" keys
+                    val isOkToRemove =
+                        KEYS_THAT_SHOULD_NOT_BE_REMOVED_WHEN_SHOP_IS_REPLACED.none { it.matches(key) }
+                    if (isOkToRemove && !answer.tags.containsKey(key)) {
+                        changes.delete(key)
+                    }
+                }
+
+                for ((key, value) in answer.tags) {
+                    changes.addOrModify(key, value)
+                }
+            }
+        }
     }
 }
