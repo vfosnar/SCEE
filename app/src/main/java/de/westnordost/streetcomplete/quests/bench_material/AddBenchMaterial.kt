4310b2161fc0f518f556cc2d68634e675f071f39
Matija Nalis <mnalis-git@voyager.hr>
2022-04-03 22:05:04 +0200

    initial bench material code

---
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bench_material/AddBenchMaterial.kt
@@ -0,0 +1,35 @@
+package de.westnordost.streetcomplete.quests.bench_material
+
+import de.westnordost.streetcomplete.R
+import de.westnordost.streetcomplete.data.osm.mapdata.Element
+import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
+import de.westnordost.streetcomplete.data.osm.mapdata.filter
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
+import de.westnordost.streetcomplete.data.osm.osmquests.Tags
+import de.westnordost.streetcomplete.data.user.achievements.QuestTypeAchievement.OUTDOORS
+import de.westnordost.streetcomplete.data.user.achievements.QuestTypeAchievement.PEDESTRIAN
+class AddBenchMaterial : OsmFilterQuestType<BenchMaterial>() {
+
+    override val elementFilter = """
+        nodes, ways with
+          amenity = bench
+          and (!area or area = no)
+          and !material
+    """
+    override val changesetComment = "Add bench material type"
+    override val wikiLink = "Tag:amenity=bench"
+    override val icon = R.drawable.ic_quest_bench_material
+    override val questTypeAchievements = listOf(PEDESTRIAN, OUTDOORS)
+
+
+    override fun getTitle(tags: Map<String, String>) = R.string.quest_benchMaterial_title
+
+    override fun getHighlightedElements(element: Element, getMapData: () -> MapDataWithGeometry) =
+        getMapData().filter("nodes, ways with amenity = bench")
+
+    override fun createForm() = AddBenchMaterialForm()
+
+    override fun applyAnswerTo(answer: BenchMaterial, tags: Tags, timestampEdited: Long) {
+        tags["material"] = answer.osmValue
+    }
+}
