f063138cda91644e33755fa97bf75f1660658c3d
Matija Nalis <mnalis-git@voyager.hr>
2022-04-03 22:20:32 +0200

    activate AddBenchMaterial, add strings

--- app/src/main/java/de/westnordost/streetcomplete/quests/QuestsModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/QuestsModule.kt
@@ -18,6 +18,7 @@ import de.westnordost.streetcomplete.quests.barrier_type.AddBarrierOnRoad
 import de.westnordost.streetcomplete.quests.barrier_type.AddBarrierType
 import de.westnordost.streetcomplete.quests.barrier_type.AddStileType
 import de.westnordost.streetcomplete.quests.bench_backrest.AddBenchBackrest
+import de.westnordost.streetcomplete.quests.bench_material.AddBenchMaterial
 import de.westnordost.streetcomplete.quests.bike_parking_capacity.AddBikeParkingCapacity
 import de.westnordost.streetcomplete.quests.bike_parking_cover.AddBikeParkingCover
 import de.westnordost.streetcomplete.quests.bike_parking_type.AddBikeParkingType
